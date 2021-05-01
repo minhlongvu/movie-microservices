076733141e8605a24d95d487ed59b2f7205a3519
Albert Vu <44858018+minhlongvu@users.noreply.github.com>
2021-05-01 13:45:55 -0500

    Changed server ports of all services. Used RestTemplate for service communication. Created Bean for RestTemplate instance.

--- movie-catalog-service/src/main/java/io/albertvu/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/albertvu/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,21 +3,43 @@ package io.albertvu.moviecatalogservice.resources;
 // accepts requests for "give me the catalog with this id" and return list of movies with everything bundled together
 
 import io.albertvu.moviecatalogservice.models.CatalogItem;
+import io.albertvu.moviecatalogservice.models.Movie;
+import io.albertvu.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId) {
-		return Collections.singletonList(
-				new CatalogItem("Transformers", "Test description", 4)
+
+		// get all rated movie IDs
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
 		);
+
+		// we can make API call with REST template using for loop or map
+		return ratings.stream().map(rating -> {
+			// get the payload at the URL and unmarshal into the class
+			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+			return new CatalogItem(movie.getName(), "Test description", rating.getRating());
+		})
+		// ToList collector can be used for collecting all Stream elements into a List instance
+		.collect(Collectors.toList());
 	}
 }
