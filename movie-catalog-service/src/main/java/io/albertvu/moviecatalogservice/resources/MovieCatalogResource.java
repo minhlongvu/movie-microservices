347f920d8128ca9d3a7f7c18fa05d137359bcba1
Albert Vu <44858018+minhlongvu@users.noreply.github.com>
2021-05-01 14:12:46 -0500

    Tested WebClient implementation of our synchronous API call

--- movie-catalog-service/src/main/java/io/albertvu/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/albertvu/moviecatalogservice/resources/MovieCatalogResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -23,6 +24,9 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable String userId) {
 
@@ -35,7 +39,14 @@ public class MovieCatalogResource {
 		// we can make API call with REST template using for loop or map
 		return ratings.stream().map(rating -> {
 			// get the payload at the URL and unmarshal into the class
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			//Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+			Movie movie = webClientBuilder.build()
+					.get()
+					.uri("http://localhost:8082/movies/" + rating.getMovieId())
+					.retrieve()
+					.bodyToMono(Movie.class)
+					.block();
 
 			return new CatalogItem(movie.getName(), "Test description", rating.getRating());
 		})
